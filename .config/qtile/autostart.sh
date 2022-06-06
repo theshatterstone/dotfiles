dc2fe64c93aac8a29f92eeebf1720bd05b527e7b
theshatterstone <90517516+theshatterstone@users.noreply.github.com>
2022-06-06 16:20:38 +0100

    Add files via upload

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash 
+
+#festival --tts $HOME/.config/qtile/welcome_msg &
+lxsession &
+#picom &
+#/usr/bin/emacs --daemon &
+#conky -c $HOME/.config/conky/doomone-qtile.conkyrc
+volumeicon &
+nm-applet &
+
+### UNCOMMENT ONLY ONE OF THE FOLLOWING THREE OPTIONS! ###
+# 1. Uncomment to restore last saved wallpaper
+#xargs xwallpaper --stretch < ~/.xwallpaper &
+# 2. Uncomment to set a random wallpaper on login
+# find /usr/share/backgrounds/dtos-backgrounds/ -type f | shuf -n 1 | xargs xwallpaper --stretch &
+# 3. Uncomment to set wallpaper with nitrogen
+nitrogen --restore &
