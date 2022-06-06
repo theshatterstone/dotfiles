810db68d6bd15c4a3e1001fa5a00bc2115cf4820
theshatterstone <90517516+theshatterstone@users.noreply.github.com>
2022-06-06 16:19:45 +0100

    Add files via upload

---
+++ bin/statusbar/arch-updates.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+    updates_arch=0
+fi
+
+if ! updates_aur=$(paru -Qum 2> /dev/null | wc -l); then
+    updates_aur=0
+fi
+
+updates=$((updates_arch + updates_aur))
+
+if [ "$updates" -gt 0 ]; then
+    echo -e "%{F#55aa55}%{F-} $updates"
+else
+    echo ""
+fi
